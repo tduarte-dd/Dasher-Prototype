1c172bae3d1505a4fd92ef32d3f4abcdd295a3fd
tduarte-dd <thiago.duarte@doordash.com>
2025-08-05 12:09:11 -0700

    Initial Commit

---
+++ Dasher Prototype/Dasher_PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dasher_PrototypeApp.swift
+//  Dasher Prototype
+//
+//  Created by Thiago Duarte on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Dasher_PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
